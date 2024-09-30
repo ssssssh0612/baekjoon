12435189c49b97e048c0720d28819de676b0d50c
sibjagun <sibjagun99@kyungmin.ac.kr>
2024-09-30 23:40:21 +0900

    Add: "백트래킹 문제풀기 애너그램 미해결"

--- src/BaekJoon/Hash/학급회장_inflearn.java
+++ src/BaekJoon/Hash/학급회장_inflearn.java
@@ -10,21 +10,64 @@ public class 학급회장_inflearn {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(br.readLine());
-        char result = 'a';
-        int maxCount = Integer.MIN_VALUE;
-        String b = br.readLine();
-        HashMap<Character,Integer> map = new HashMap<>();
-        for (int i = 0; i < b.length(); i++) {
-            map.put(b.charAt(i),map.getOrDefault(b.charAt(i), 0) + 1 );
+        String str = br.readLine();
+        HashMap<Character,Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < a; i++) {
+            char ch = str.charAt(i);
+            hashMap.put(ch,hashMap.getOrDefault(ch,0)+1);
         }
-
-        for (Character key : map.keySet()) {
-            int count = map.get(key);
-            if (count > maxCount) {
-                maxCount = Math.max(count, maxCount);
-                result = key;
+        int max = Integer.MIN_VALUE;
+        char result = ' ';
+        for(Character ch : hashMap.keySet()){
+//            System.out.println(hashMap.get(ch));
+            if( max < hashMap.get(ch)){
+                max = hashMap.get(ch);
+                result = ch;
             }
         }
         System.out.println(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        char result = 'a';
+//        int maxCount = Integer.MIN_VALUE;
+//        String b = br.readLine();
+//        HashMap<Character,Integer> map = new HashMap<>();
+//        for (int i = 0; i < b.length(); i++) {
+//            map.put(b.charAt(i),map.getOrDefault(b.charAt(i), 0) + 1 );
+//        }
+//
+//        for (Character key : map.keySet()) {
+//            int count = map.get(key);
+//            if (count > maxCount) {
+//                maxCount = Math.max(count, maxCount);
+//                result = key;
+//            }
+//        }
+//        System.out.println(result);
     }
 }
---
+++ src/BaekJoon/구현/프린터큐_1966.java
@@ -0,0 +1,15 @@
+package BaekJoon.구현;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 프린터큐_1966 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int caseCount = Integer.parseInt(br.readLine());
+        for (int i = 0; i < caseCount; i++) {
+            
+        }
+    }
+}
