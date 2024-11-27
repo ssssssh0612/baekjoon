af2614f2beda9f6fcad6019d23cc35c5780e864c
sibjagun <sibjagun99@kyungmin.ac.kr>
2024-11-27 21:16:03 +0900

    Add : 베라의 패션 문제 해결

---
+++ src/BaekJoon/구현/베라의패션_15439.java
@@ -0,0 +1,13 @@
+package BaekJoon.구현;
+
+import java.io.*;
+
+public class 베라의패션_15439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n*(n-1));
+
+    }
+}
