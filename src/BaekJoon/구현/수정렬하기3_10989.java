8d44b7f596902963e09dcec65febce7e2fbbc6ae
sibjagun <sibjagun99@kyungmin.ac.kr>
2024-12-30 18:43:00 +0900

    Add :"정렬 문제 우선순위 큐로해결"

---
+++ src/BaekJoon/구현/수정렬하기3_10989.java
@@ -0,0 +1,22 @@
+package BaekJoon.구현;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class 수정렬하기3_10989 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < testCase; i ++){
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!pq.isEmpty()){
+            sb.append(pq.poll()).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
