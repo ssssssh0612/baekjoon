0cb2ca9b9a56b65a612537456bee380edb7b4c8a
sibjagun <sibjagun99@kyungmin.ac.kr>
2025-01-14 10:53:31 +0900

    Add :"투포인터 문제 해결(답봄)"

---
+++ src/BaekJoon/TwoPointer/문자열교환_1522.java
@@ -0,0 +1,39 @@
+package BaekJoon.TwoPointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 문자열교환_1522 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int aCount = 0;
+        int result = Integer.MAX_VALUE;
+        for(int i = 0 ; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch == 'a'){
+                aCount++;
+            }
+        }
+        for(int i = 0; i < str.length(); i++){
+            int end = i + aCount;
+            int bCount = 0;
+            for(int j = i; j < end; j++){
+                if(j >= str.length()){
+                    char ch = str.charAt(j - str.length());
+                    if(ch == 'b'){
+                        bCount++;
+                    }
+                }else{
+                    char ch = str.charAt(j);
+                    if (ch == 'b') {
+                        bCount++;
+                    }
+                }
+            }
+            result = Math.min(bCount, result);
+        }
+        System.out.println(result);
+    }
+}
