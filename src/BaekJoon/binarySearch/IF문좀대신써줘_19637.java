a3ede0abb007d01f632d442afe6d886c88d4fc3f
sibjagun <sibjagun99@kyungmin.ac.kr>
2025-04-08 16:29:09 +0900

    Add: "헷갈리는 알고리즘 이분탐색 정리 후 문제 복습"

--- src/BaekJoon/binarySearch/IF문좀대신써줘_19637.java
+++ src/BaekJoon/binarySearch/IF문좀대신써줘_19637.java
@@ -7,40 +7,40 @@ import java.util.StringTokenizer;
 
 public class IF문좀대신써줘_19637 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        StringBuilder sb=new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        int N=Integer.parseInt(st.nextToken());
-        int M=Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        String[][] title=new String[N][2];
+        String[][] title = new String[n][2];
 
-        for(int i=0;i<N;i++){
-            st=new StringTokenizer(br.readLine());
-            title[i][0]=st.nextToken();
-            title[i][1]=st.nextToken();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            title[i][0] = st.nextToken();
+            title[i][1] = st.nextToken();
         }
 
-        for(int i=0;i<M;i++){
-            int num=Integer.parseInt(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int num = Integer.parseInt(br.readLine());
 
-            int left=0;
-            int right=N-1;
-            int mid=(left+right)/2;
-            while(left<=right){
-                mid=(left+right)/2;
+            int left = 0;
+            int right = n;
+            int mid = 0;
 
-                int titleInt=Integer.parseInt(title[mid][1]);
-                if(titleInt<num){
-                    left=mid+1;
-                }
-                else{
-                    right=mid-1;
+            while (left < right) {
+                mid = (left + right) / 2;
+
+                int titleInt = Integer.parseInt(title[mid][1]);
+                if (titleInt < num) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
                 }
             }
 
-            sb.append(title[left][0]+"\n");
+            sb.append(title[left][0]).append("\n");
         }
 
         System.out.print(sb);
