6412e140bdd63b4ddded4f97142aa0361598c5ea
sibjagun <sibjagun99@kyungmin.ac.kr>
2025-10-01 14:27:34 +0900

    Add: "문자열, 시뮬레이션 문제 해결"

--- src/BaekJoon/구현/졸려_9519.java
+++ src/BaekJoon/구현/졸려_9519.java
@@ -1,36 +1,62 @@
 package BaekJoon.구현;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class 졸려_9519 {
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int n=sc.nextInt();
-        int m=sc.nextInt();
-
-        Deque<Integer> queue=new LinkedList<>();
-        for(int i = 1; i<=n ;i++){
-            queue.add(i);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        List<String> list = new ArrayList<>();
+        list.add(str);
+        Set<String> set = new HashSet<>();
+        set.add(str);
+//        StringBuilder sb = new StringBuilder("ABCDE");
+//        sb.delete(sb.length()  - 2, sb.length() - 1);
+//        System.out.println(sb);
+        int size = set.size();
+        while(true){
+            StringBuilder sb = new StringBuilder(list.get(list.size() - 1));
+            char[] ch = new char[sb.length() / 2];
+            int index = sb.length() - 1;
+            for (int i = 0; i < ch.length; i++) {
+                ch[i] = sb.toString().charAt(index);
+                index--;
+            }
+//             뒤에 두글자
+//            System.out.println(sb);
+            sb.delete(sb.length() - ch.length, sb.length());
+//            System.out.println(sb);
+            int addIndex = 1;
+            for (int i = 0; i < ch.length; i++) {
+                sb.insert(addIndex, ch[i]);
+                addIndex += 2;
+//                System.out.println(sb);
+            }
+            set.add(sb.toString());
+            if(set.size() != size){
+                size = set.size();
+                list.add(sb.toString());
+            }else{
+                break;
+            }
         }
-        List<Integer> list=new ArrayList<>();
-        while(!queue.isEmpty()){
-            for (int i = 0; i < m; i++) {
-                if( i == m - 1){
-                    list.add(queue.poll());
-                }else{
-                    int now = queue.poll();
-                    queue.addLast(now);
+        int result = num % size;
+//        System.out.println(result);
+        if(result == 0){
+            System.out.println(str);
+        }else{
+            int index = 0;
+            for (int i = list.size() - 1; i >= 0 ; i--) {
+                index++;
+                if(index == result){
+                    System.out.println(list.get(i));
+                    return;
                 }
             }
         }
-        System.out.print("<");
-        for (int i = 0; i < list.size(); i++) {
-            if(i == list.size()-1){
-                System.out.print(list.get(i));
-            }else{
-                System.out.print(list.get(i)+", ");
-            }
-        }
-        System.out.print(">");
     }
 }
