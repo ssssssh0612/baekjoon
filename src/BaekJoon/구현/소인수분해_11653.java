81bb9075d04f64c4887d1962037d69ec5e8f1f14
sibjagun <sibjagun99@kyungmin.ac.kr>
2025-11-10 23:44:46 +0900

    Add: "자바 기본문제 해결"

---
+++ src/BaekJoon/구현/소인수분해_11653.java
@@ -0,0 +1,26 @@
+package BaekJoon.구현;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 소인수분해_11653 {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            int n = Integer.parseInt(br.readLine());
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                while (n % i == 0) {
+                    sb.append(i).append('\n');
+                    n /= i;
+                }
+            }
+            if (n != 1) {
+                sb.append(n);
+            }
+            System.out.println(sb);
+        }
+    }
+}
